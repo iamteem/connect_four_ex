fc8fc44f8af7e204471dcb66e8eda34cfc1ade4a
Tim Medina <iamteem@gmail.com>
2018-08-15 01:20:48 +0800

    WIP

--- assets/js/game/board.js
+++ assets/js/game/board.js
@@ -1,10 +1,13 @@
 export class Board {
   constructor(stage) {
     this.stage = stage;
+    this.cellWidth = 80;
+    this.cellHoleRadius = 28;
   }
 
   render() {
-    let width = 80, radius = 28;
+    let width = this.cellWidth,
+        radius = this.cellHoleRadius;
     this.boardContainer = new createjs.Container();
     for (let row = 0; row < 6; row++) {
       for (let col = 0; col < 7; col++) {
@@ -17,9 +20,36 @@ export class Board {
           ));
       }
     }
-    this.boardContainer.y = 50;
+    this.boardContainer.y = this.cellWidth;
     this.stage.addChild(this.boardContainer);
     this.stage.update();
+
+    this.stage.addEventListener('stagemousemove', e => {
+      let column = this.getTargetedColumn(e.stageX, e.stageY);
+      if (column != this.targetColumn) {
+        this.clearShadowDisk();
+        this.showShadowDiskAtColumn(column);
+        this.targetColumn = column;
+        this.stage.update(e);
+      }
+    });
+  }
+
+  getTargetedColumn(stageX, stageY) {
+    let point = this.boardContainer.globalToLocal(stageX, stageY);
+    return Math.floor(point.x / this.cellWidth);
+  }
+
+  showShadowDiskAtColumn(column) {
+    let g = new createjs.Graphics();
+    g.beginFill('gray').
+      drawCircle(column * this.cellWidth + this.cellWidth / 2, this.cellWidth / 2 + 4, this.cellWidth / 2 - 4);
+    this.shadowDisk = new createjs.Shape(g);
+    this.stage.addChild(this.shadowDisk);
+  }
+
+  clearShadowDisk() {
+    this.stage.removeChild(this.shadowDisk);
   }
 
   renderDropButtons() {
@@ -39,4 +69,3 @@ export class Board {
     return new createjs.Shape(g);
   }
 }
-
